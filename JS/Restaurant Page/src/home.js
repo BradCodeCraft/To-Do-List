b8db4f73be3d326444b706fb4812c2890a0c77f5
BradCodeCraft <bradcodecraft@gmail.com>
2024-12-27 20:26:58 -0500

    Restaurant Page

--- JS/Restaurant Page/src/home.js
+++ JS/Restaurant Page/src/home.js
@@ -1,14 +1,10 @@
-const homeContainer = document.createElement('div');
-
 const home = document.createElement('div');
 home.classList.add('home');
 const homeText = document.createElement('h1');
 homeText.textContent = 'A Generic Restaurant';
 const homeImage = document.createElement('img');
 homeImage.classList.add('home-image');
-homeImage.src = "https://static.vecteezy.com/system/resources/previews/027/536/034/non_2x/restaurant-food-restaurant-food-top-view-ai-generative-free-png.png"
-
+homeImage.src = "https://images.unsplash.com/photo-1506084868230-bb9d95c24759?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 home.append(homeText, homeImage);
-homeContainer.appendChild(home);
 
-module.exports = homeContainer;
+module.exports = home;

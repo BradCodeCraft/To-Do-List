228b2bb72ceef1d1ba819eb3bd8945374fcd124d
bradcodecraft <bradcodecraft@gmail.com>
2025-01-01 21:43:19 -0500

    pre-alpha: restart

--- JS/To Do List/src/index.js
+++
@@ -1,7 +0,0 @@
-/* ========== IMPORTS ========== */
-require("./styles.css");
-const navbar = require("./js/navbar.js");
-const today = require("./js/today.js");
-
-/* ========== BODY ========== */
-document.body.append(navbar, today);
